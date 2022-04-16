55583ed3ae0437ddf4af9949d973d21c509e7f7c
Flixirr <filip.0krezel@gmail.com>
2022-04-16 14:40:03 +0200

    Finished models

--- app/models.py
+++ app/models.py
@@ -3,33 +3,47 @@ from sqlalchemy.orm import relationship
 from flask_appbuilder import Model
 
 class City(Model):
-    id = Column(Integer, primary_key=True)
+    city_id = Column(Integer, primary_key=True)
     name = Column(String(50), unique = True, nullable=False)
 
     def __repr__(self):
-        return self.id + ' ' + self.name
+        return self.city_id + ' ' + self.name
 
 class Route(Model):
-    id = Column(String(10), primary_key=True)
+    route_id = Column(String(10), primary_key=True)
     short_name = Column(String(50))
     description = Column(String(255))
 
     def __repr__(self):
-        return self.id + ' ' + self.short_name + ' ' + self.description
+        return self.route_id + ' ' + self.short_name + ' ' + self.description
 
 class Stop(Model):
-    id = Column(Integer, primary_key=True)
+    stop_id = Column(Integer, primary_key=True)
     code = Column(Integer)
     name = Column(String(255))
     latitude = Column(Float)
     longitude = Column(Float)
 
     def __repr__(self):
-        return self.id + ' ' + self.code + ' ' + self.name
+        return self.stop_id + ' ' + self.code + ' ' + self.name
 
 class Trip(Model):
-    route_id = Column(String(10), ForeignKey('route.id'))
-    service_id = Column(Integer, )
+    route_id = Column(String(10), ForeignKey('route.route_id'))
+    route = relationship("Route")
+    service_id = Column(Integer)
+    trip_id = Column(String(20), primary_key=True)
+    trip_headsign = Column(String(255))
+    direction_id = Column(Integer)
+
+    def __repr__(self):
+        return self.trip_id + ' ' + self.route_id + ' ' + self.trip_headsign + ' ' + self.direction_id
 
 class StopTimes(Model):
-    id = Column
+    trip_id = Column(String(20), ForeignKey('trip.trip_id'))
+    trip = relationship("Trip")
+    stop_id = Column(Integer, ForeignKey('stop.stop_id'))
+    stop = relationship("Stop")
+    departure_time = Column(DateTime)
+
+    def __repr__(self):
+        return self.trip_id + ' ' + self.stop_id + ' ' + self.departure_time
