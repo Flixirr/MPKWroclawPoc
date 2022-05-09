58225f184d67f0d957939c63f56b9244a8bd9e85
Flixirr <filip.0krezel@gmail.com>
2022-05-09 19:17:09 +0200

    Finished last repo

--- app/blueprints/closest_bp.py
+++ app/blueprints/closest_bp.py
@@ -43,8 +43,8 @@ class CalculateDistance(MethodView):
             return Response({'Latitude does not meet requirements: <-90, 90>'}, status=400)
 
         near_stops = Stop.objects(lat_lon__near=[lat, lon], lat_lon__max_distance=distance)[:5]
-        print(departure_time)
-        stops_at_given_time = StopTimes.objects(departure_time__gte=departure_time, stop__in=near_stops)[0]
+
+        stops_at_given_time = StopTimes.objects(departure_time__gte=departure_time, stop__in=near_stops).order_by('departure_time')[:5]
 
         response_json = stops_at_given_time.to_json()
 
